ae5a15613e55d1fe1b21f2b2ccdadac65c61c1f9
Philippe Prados <philippe.prados@gmail.com>
2011-11-24 10:45:01 +0100

    Patch pour démo + Intégration discover

--- src/org/remoteandroid/internal/Constants.java
+++ src/org/remoteandroid/internal/Constants.java
@@ -28,7 +28,7 @@ public class Constants
 	public static final boolean USE_SHAREDLIB					=Friend.USE_SHAREDLIB;
 	public static final String SHARED_LIB						=Friend.SHARED_LIB;
 		
-	public static final int BINDING_TIMEOUT_WAIT				=3000;
+	public static final int BINDING_TIMEOUT_WAIT				=3000; // FIXME
 	public static final int BINDING_NB_RETRY					=3;
 	
 	/** Buffer size for down load */
@@ -42,16 +42,16 @@ public class Constants
 	/** Order uris with Bluetooth first */
 	public static final boolean BLUETOOTH_FIRST					=false;
 	/** Auto-propose pairing with remote BT device. */
-	public static final boolean AUTO_PAIRING_BT			=true;
+	public static final boolean AUTO_PAIRING_BT					=true;
 	
-	public static final int BT_HACK_WAIT_AFTER_CREATE_RF_COMM=0; // FIXME: 0; // Zero for good bluetooth stack.
+	public static final int BT_HACK_WAIT_AFTER_CREATE_RF_COMM	=0; // FIXME: 0; // Zero for good bluetooth stack.
     /** Delay to discover others remote androids. */
 	public static final long TIME_MAX_TO_DISCOVER				=20000L;
 
 	/** Use ethernet */
 	public static final boolean ETHERNET						=true;
     /** Use only IP v4 */
-    public static final boolean ETHERNET_ONLY_IPV4 				=false; // FIXME: Socket level invalide depuis Motorola en IPV6. Xoom: No route to host
+    public static final boolean ETHERNET_ONLY_IPV4 				=false; 
 	/** Order uris with IPV4 before IPV6 */
 	public static final boolean ETHERNET_IPV4_FIRST				=false;
     
@@ -66,17 +66,21 @@ public class Constants
 	public static final int BT_NB_UUID							=5;
 
 	/** Prefix for log */
-	public static final String PREFIX_LOG='['+Build.DEVICE+' '+Build.MODEL+"] ";
+	public static final String PREFIX_LOG='['/*+Build.DEVICE+' '*/+Build.MODEL+"] ";
 
 	/** Timeout for ping remote binder. */
-	public static final long TIMEOUT_PING_BINDER				=2000L; // second
+	public static final long TIMEOUT_PING_BINDER				=5000L; // second
 
 	/** Timeout for connect to remote android. */
-	public static final long TIMEOUT_CONNECT					=2000L; // second
+	public static final long TIMEOUT_CONNECT					=10000L; // FIXME second
+    /** Flush current data before close the socket. */
+    public static final boolean ETHERNET_SO_LINGER					=true; 		// Vide les derniers paquets avant la fermeture du socket
+    /** Timeout to flush the last datas. */
+    public static final int ETHERNET_SO_LINGER_TIMEOUT				=50000; 		// FIXME Delay for flush last packets
 	/** Timeout to detect if binder is alive. */
-	public static final long TIMEOUT_IS_BINDER_ALIVE			=5000L; // second
+	public static final long TIMEOUT_IS_BINDER_ALIVE			=20000L; // FIXME second
 	/** Timeout to inform the finalize of binder. */
-	public static final long TIMEOUT_FINALIZE					=2000L; // second
+	public static final long TIMEOUT_FINALIZE					=10000L; // FIXME second
 	
 	/** Timeout for accept a pairing chalenge. */
 	public static final long TIMEOUT_PAIRING_ASK_CHALENGE		=30000L; // 30s
